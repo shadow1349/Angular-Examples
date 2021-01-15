4e1e35ddb5b0b5543d02d7f8394a0094e2b6442b
Sam Redmond <sam.redmond@homesite.com>
2021-01-15 11:34:23 -0600

    Adding basic routing as an example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/page-one',
+    pathMatch: 'full',
+  },
+  {
+    path: 'page-one',
+    loadChildren: () =>
+      import('./page-one/page-one.module').then((mod) => mod.PageOneModule),
+  },
+  {
+    path: 'page-two',
+    loadChildren: () =>
+      import('./page-two/page-two.module').then((mod) => mod.PageTwoModule),
+  },
+  {
+    path: '404',
+    loadChildren: () =>
+      import('./not-found/not-found.module').then((mod) => mod.NotFoundModule),
+  },
+  { path: '**', redirectTo: '/404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
