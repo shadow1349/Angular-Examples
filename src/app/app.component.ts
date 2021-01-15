7a6ce0bebb58562db0204ab687e78ca6721d8673
Sam Redmond <sam.redmond@homesite.com>
2021-01-15 10:15:24 -0600

    Creating form in component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  myForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl(''),
+  });
+
+  submit(): void {
+    if (this.myForm.invalid) {
+      alert('Oops your form is invalid!');
+      return;
+    }
+
+    // Do something with the values
+    console.log('EMAIL: ', this.myForm.get('email')?.value);
+    console.log('FIRST NAME: ', this.myForm.get('firstName')?.value);
+    console.log('LAST NAME: ', this.myForm.get('lastName')?.value);
+    alert('Thanks!');
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
